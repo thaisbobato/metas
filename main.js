96ed3287f76b51d12a31d279844a304597af4dde
Thais Bobato <105284617+thaisbobato@users.noreply.github.com>
2024-03-14 10:23:50 -0300

    ok

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const botoes = document.querySelectorAll(".botao");
 
-for (Let i = 0; i < botoes.length; i++) {
+for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclik = function () {
 
-        for (Let j = 0; j < botoes.length; j++) {
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
         }
 
